150a621bdd645047859c360817773c411cbbb011
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-02-21 17:32:10 -0800

    required dotenv

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require("mongoose");
 
 var router = require('./routes/index');
 
