ada818e930289303734e912ad8633392f505afda
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-02-21 16:43:17 -0800

    made outline for user route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,35 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
+
+router.post("/", (req, res, next) => {
+  res.send("add new user to db");
+});
+
+router.put(
+  "/:id",
+  (req, res, next) => {
+    console.log("verify token");
+    next();
+  },
+  (req, res, next) => {
+    res.send("edit user's info");
+  }
+);
+
+router.delete(
+  "/:id",
+  (req, res, next) => {
+    console.log("verify token");
+    next();
+  },
+  (req, res, next) => {
+    res.send("delete existing user");
+  }
+);
 
 module.exports = router;
