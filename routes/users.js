c7ffbcd42d7d80efb1169d24740380e47d32c291
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-02-21 16:50:07 -0800

    added extra authentication to outline

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,27 @@ var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
-});
+router.get(
+  "/",
+  (req, res, next) => {
+    console.log("verify token");
+    next();
+  },
+  function (req, res, next) {
+    res.send("respond with a resource");
+  }
+);
+
+router.get(
+  "/:id",
+  (req, res, next) => {
+    console.log("verify token");
+    next();
+  },
+  (req, res, next) => {
+    res.send("find by id and return");
+  }
+);
 
 router.post("/", (req, res, next) => {
   res.send("add new user to db");
