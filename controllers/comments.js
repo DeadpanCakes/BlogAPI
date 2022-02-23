505df31b59660d7efc7e9a206760ff6c6eaa466b
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-02-22 17:56:27 -0800

    made postComment

---
+++ controllers/comments.js
@@ -0,0 +1,27 @@
+const async = require("async");
+
+const Comment = require("../models/comment");
+const User = require("../models/user");
+
+const sanitize = (req, res, next) => {
+  console.log("sanitize here");
+  next();
+};
+
+module.exports.postComment = [
+  sanitize,
+  (req, res, next) => {
+    const timestamp = new Date();
+    const newComment = new Comment({
+      timestamp,
+      content: req.body.content,
+      author: req.user,
+      parent: req.body.parent,
+      commentOf: req.params.postid,
+    });
+    newComment.save((err) => {
+      if (err) next(err);
+      res.json(newComment.url);
+    });
+  },
+];
