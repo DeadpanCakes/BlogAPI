9e9fca4f549bcfbcbc9f880ba6f6fff777c3c635
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-02-23 15:42:18 -0800

    replaced placeholder sanitize fn w validation and sanitization

--- controllers/users.js
+++ controllers/users.js
@@ -1,13 +1,13 @@
+const { body, validationResult } = require("express-validator");
+
 const User = require("../models/user");
 
-const sanitize = (req, res, next) => {
-  console.log("sanitize input");
-  next();
-};
-
 module.exports.postUser = [
-  sanitize,
+  body("username", "Username Required").trim().isLength({ min: 1 }).escape(),
+  body("firstName", "First Name Required").trim().isLength({ min: 1 }).escape(),
+  body("lastName", "Last Name Required").trim().isLength({ min: 1 }).escape(),
   (req, res, next) => {
+    const errors = validationResult(req);
     const { username, firstName, lastName } = req.body;
     console.log(req.body);
     User.findOne({ username }).exec((err, existingUser) => {
@@ -16,51 +16,66 @@ module.exports.postUser = [
         console.log(existingUser);
         res.send("username taken");
       } else {
-        const newUser = new User({
-          username,
-          firstName,
-          lastName,
-          isAdmin: false,
-        });
-        newUser.save((err) => {
-          if (err) next(err);
-          res.redirect("/");
-        });
+        if (!errors.isEmpty()) {
+          res.json({
+            username,
+            firstName,
+            lastName,
+            errors: errors.array().map((error) => error.msg),
+          });
+        } else {
+          const newUser = new User({
+            username,
+            firstName,
+            lastName,
+            isAdmin: false,
+          });
+          newUser.save((err) => {
+            if (err) next(err);
+            res.redirect("/users");
+          });
+        }
       }
     });
   },
 ];
 
-module.exports.getUser = [
-  sanitize,
-  (req, res, next) => {
-    User.findById(req.params.id).exec((err, user) => {
-      if (err) next(err);
-      res.json(user);
-    });
-  },
-];
+module.exports.getUser = (req, res, next) => {
+  User.findById(req.params.id).exec((err, user) => {
+    if (err) next(err);
+    res.json(user);
+  });
+};
 
-module.exports.getUsers = [
-  sanitize,
-  (req, res, next) => {
-    User.find((err, users) => {
-      if (err) next(err);
-      res.json(users);
-    });
-  },
-];
+module.exports.getUsers = (req, res, next) => {
+  User.find((err, users) => {
+    if (err) next(err);
+    res.json(users);
+  });
+};
 
 module.exports.updateUser = [
-  sanitize,
+  body("username", "Username Required").trim().isLength({ min: 1 }).escape(),
+  body("firstName", "First Name Required").trim().isLength({ min: 1 }).escape(),
+  body("lastName", "Last Name Required").trim().isLength({ min: 1 }).escape(),
   (req, res, next) => {
-    User.findByIdAndUpdate(req.params.id, req.body).exec((err) => {
-      if (err) next(err);
-      User.findById(req.params.id).exec((err, user) => {
-        if (err) next(err);
-        res.json(user);
+    const errors = validationResult(req.body);
+    if (errors.isEmpty()) {
+      res.send({
+        username: req.body.username,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        errors: errors.array().map((error) => error.msg),
       });
-    });
+    } else {
+      User.findByIdAndUpdate(req.params.id, req.body).exec((err) => {
+        if (err) next(err);
+        User.findById(req.params.id).exec((err, user) => {
+          if (err) next(err);
+          res.json(user);
+        });
+      });
+    }
   },
 ];
 
