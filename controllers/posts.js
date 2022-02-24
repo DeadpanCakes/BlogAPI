899db48d7036ca4dfac2ee66b95c8c2847e8cbb8
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-02-23 16:05:35 -0800

    replaced sanitize method with express-validator

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,27 +1,41 @@
+const { body, validationResult } = require("express-validator");
 const Post = require("../models/post");
 
-const sanitize = (req, res, next) => {
-  console.log("sanitize here");
-  next();
-};
-
-module.exports.postPost = (req, res, next) => {
-  const { isPublished, tags, content, title } = req.body;
-  const timestamp = new Date();
-  const newPost = new Post({
-    isPublished,
-    timestamp,
-    tags,
-    author: req.user,
-    lastUpdate: timestamp,
-    content,
-    title,
-  });
-  newPost.save((err) => {
-    if (err) next(err);
-    res.redirect(newPost.url);
-  });
-};
+module.exports.postPost = [
+  body("content", "Post Must Be Filled Out")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("title", "Post Must Have Title").trim().isLength({ min: 1 }).escape(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    const { isPublished, tags, content, title } = req.body;
+    if (!errors.isEmpty()) {
+      res.json({
+        isPublished,
+        tags,
+        content,
+        title,
+        errors: errors.array().map((error) => error.msg),
+      });
+    } else {
+      const timestamp = new Date();
+      const newPost = new Post({
+        isPublished,
+        timestamp,
+        tags,
+        author: req.user,
+        lastUpdate: timestamp,
+        content,
+        title,
+      });
+      newPost.save((err) => {
+        if (err) next(err);
+        res.redirect(newPost.url);
+      });
+    }
+  },
+];
 
 module.exports.getPosts = (req, res, next) => {
   Post.find().exec((err, posts) => {
@@ -37,15 +51,33 @@ module.exports.getPost = (req, res, next) => {
   });
 };
 
-module.exports.updatePost = (req, res, next) => {
-  Post.findByIdAndUpdate(req.params.id, req.body).exec((err, post) => {
-    if (err) next(err);
-    Post.findById(req.params.id).exec((err, post) => {
-      if (err) next(err);
-      res.json(post);
-    });
-  });
-};
+module.exports.updatePost = [
+  body("content", "Post Must Be Filled Out")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("title", "Post Must Have Title").trim().isLength({ min: 1 }).escape(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.json({
+        isPublished,
+        tags,
+        content,
+        title,
+        errors: errors.array().map((error) => error.msg),
+      });
+    } else {
+      Post.findByIdAndUpdate(req.params.id, req.body).exec((err) => {
+        if (err) next(err);
+        Post.findById(req.params.id).exec((err, post) => {
+          if (err) next(err);
+          res.json(post);
+        });
+      });
+    }
+  },
+];
 
 module.exports.deletePost = (req, res, next) => {
   Post.findByIdAndDelete(req.params.id).exec((err) => {
