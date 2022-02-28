f27af96996bfb46d390d9ffd1d4c25f4d2ee5804
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-02-28 14:26:54 -0800

    used passport with Local Strategy

--- app.js
+++ app.js
@@ -1,50 +1,70 @@
 require("dotenv").config();
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 const mongoose = require("mongoose");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const bcrypt = require("bcrypt");
 
-var router = require('./routes/index');
+var router = require("./routes/index"); 
+const User = require("./models/user");
 
 var app = express();
 
-const {DEV_DB_URL} = process.env;
+const { DEV_DB_URL } = process.env;
 const mongoDB = process.env.MONGO_URI || DEV_DB_URL;
-mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error"));
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+passport.use(
+  new LocalStrategy((username, password, next) => {
+    User.findOne({ username }).exec((err, user) => {
+      if (err) next(err);
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (err) next(err);
+        if (res) {
+          return next(null, user);
+        } else {
+          return next(null, false, { message: "Wrong Password" });
+        }
+      });
+    });
+  })
+);
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/api/users', router.users);
+app.use("/api/users", router.users);
 app.use("/api", router.comments);
 app.use("/api/posts", router.posts);
-app.use("/", (req, res) => res.send("Home Page"))
+app.use("/", (req, res) => res.send("Home Page"));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
