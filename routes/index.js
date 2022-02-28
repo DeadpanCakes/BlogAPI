50a344b2746acfd950be77be97c18bbe45a3f8fc
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-02-28 15:15:41 -0800

    hooked up login to route and exported

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ var router = express.Router();
 const users = require("./users");
 const comments = require("./comments");
 const posts = require("./posts");
+const login = require('./login');
 
-module.exports = { users, comments, posts };
+module.exports = { users, comments, posts, login };
