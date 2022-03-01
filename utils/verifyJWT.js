2315c76c922411d466fb30fde3f9254c7e2b4301
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-02-28 16:26:56 -0800

    check if bearerHeader is undefined

--- utils/verifyJWT.js
+++ utils/verifyJWT.js
@@ -1,10 +1,14 @@
 const verifyToken = (req, res, next) => {
   const bearerHeader = req.headers["authorization"];
-  if (bearerHeader.startsWith("Bearer ")) {
+  if (bearerHeader) {
+    if (bearerHeader.startsWith("Bearer")) {
+      console.log(bearerHeader)
       req.token = bearerHeader.split(" ")[1];
-      next()
+      //save token in local storage
+      next();
+    }
   } else {
-      res.sendStatus(403)
+    res.sendStatus(403);
   }
 };
 
