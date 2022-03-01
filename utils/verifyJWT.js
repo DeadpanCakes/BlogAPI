daf22f44a29d06a4a1466819cd9b01dd3b34c1d4
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-03-01 14:32:59 -0800

    rm console log

--- utils/verifyJWT.js
+++ utils/verifyJWT.js
@@ -2,7 +2,6 @@ const verifyToken = (req, res, next) => {
   const bearerHeader = req.headers["authorization"];
   if (bearerHeader) {
     if (bearerHeader.startsWith("Bearer")) {
-      console.log(bearerHeader)
       req.token = bearerHeader.split(" ")[1];
       //save token in local storage
       next();
