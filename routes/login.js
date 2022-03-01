da7167027d299f3545f8436a39f172568e954713
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-02-28 17:30:03 -0800

    added get placeholder for when authentication fails

--- routes/login.js
+++ routes/login.js
@@ -3,6 +3,10 @@ const express = require("express");
 const loginController = require("../controllers/login");
 const router = express.Router();
 
+router.get("/", (req, res) => {
+    res.send("please post credentials")
+})
+
 router.post("/", loginController.login);
 
 module.exports = router
