141fa1faf69e07ac41f11794b78fc997402db76d
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-03-01 16:37:59 -0800

    replaced placeholder verify Token middleware

--- routes/comments.js
+++ routes/comments.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const router = express.Router();
+const verifyToken = require("../utils/verifyJWT");
 
 router.get("/posts/:postid/comments", (req, res, next) => {
   res.send("return all comments associated with post");
@@ -10,23 +11,13 @@ router.get("/posts/:postid/comments/commentid", (req, res, next) => {
   res.send("return specific comment");
 });
 
-router.post(
-  "/posts/:postid/comments",
-  (req, res, next) => {
-    console.log("verify token");
-    next();
-  },
-  (req, res, next) => {
-    res.send("add new comment to db; update post with new comment");
-  }
-);
+router.post("/posts/:postid/comments", verifyToken, (req, res, next) => {
+  res.send("add new comment to db; update post with new comment");
+});
 
 router.put(
   "/posts/:postid/comments/commentid",
-  (req, res, next) => {
-    console.log("verify token as user or admin");
-    next();
-  },
+  verifyToken,
   (req, res, next) => {
     res.send("update comment");
   }
@@ -34,10 +25,7 @@ router.put(
 
 router.delete(
   "/posts/:postid/comments/commentid",
-  (req, res, next) => {
-    console.log("verify token as user or admin");
-    next();
-  },
+  verifyToken,
   (req, res, next) => {
     console.log("delete comment");
   }
