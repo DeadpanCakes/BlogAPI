b9fc70382e200b8a51cb66d846891a247925a733
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-03-01 17:09:11 -0800

    replaced placeholder controller methods with real ones

--- routes/comments.js
+++ routes/comments.js
@@ -1,34 +1,29 @@
 const express = require("express");
 
 const router = express.Router();
+const commentController = require("../controllers/comments");
 const verifyToken = require("../utils/verifyJWT");
 
-router.get("/posts/:postid/comments", (req, res, next) => {
-  res.send("return all comments associated with post");
-});
+router.get("/posts/:postid/comments", commentController.getComments);
 
-router.get("/posts/:postid/comments/commentid", (req, res, next) => {
-  res.send("return specific comment");
-});
+router.get("/posts/:postid/comments/commentid", commentController.getComment);
 
-router.post("/posts/:postid/comments", verifyToken, (req, res, next) => {
-  res.send("add new comment to db; update post with new comment");
-});
+router.post(
+  "/posts/:postid/comments",
+  verifyToken,
+  commentController.postComment
+);
 
 router.put(
   "/posts/:postid/comments/commentid",
   verifyToken,
-  (req, res, next) => {
-    res.send("update comment");
-  }
+  commentController.updateComment
 );
 
 router.delete(
   "/posts/:postid/comments/commentid",
   verifyToken,
-  (req, res, next) => {
-    console.log("delete comment");
-  }
+  commentController.deleteComment
 );
 
 module.exports = router;
